9fd18df529ce6e7d49cafa3ad28adf8fd7f6694a
Sergio Gil Guerrero García <30884294+SergilGG@users.noreply.github.com>
2023-06-11 21:45:04 -0600

    Example 2

---
+++ Redundancia2.m
@@ -0,0 +1,43 @@
+clc
+
+% Crear una matriz de datos de ejemplo
+data = rand(10, 4); %(filas, columnas)
+disp(data);
+
+% Reservar espacio para los resultados
+sums = zeros(1, size(data, 1));
+
+% Configurar el número de hilos
+numThreads = 4;
+
+% Verificar si hay un pool de trabajadores abierto, si no lo hay, crear uno con el número de hilos indicado
+if isempty(gcp('nocreate'))
+    parpool(numThreads);
+end
+
+% Crear una copia de los datos para la replicación
+dataCopy = data;
+
+% Ejecutar cálculos en paralelo utilizando parfor
+parfor i = 1:size(data, 1)
+    try
+        % Simular un fallo en un hilo específico (por ejemplo, el hilo 2)
+        if i == 2 %|| 8
+            disp('Error');
+            error('Simulated failure in current thread');
+        end
+
+        % Calcular la suma de la fila i
+        sums(i) = sum(data(i, :));
+    catch err
+        % Manejar la excepción (en este caso, simplemente mostrar un mensaje de error)
+        disp(['Error en el ciclo ', num2str(i), ': ', err.message,'-> ',  num2str(getCurrentTask().ID)]);
+        sums(i) = sum(dataCopy(i, :));
+    end
+end
+
+% Mostrar los resultados
+disp(sums);
+
+%Elimina el pool de threads creado
+delete(gcp('nocreate'));
